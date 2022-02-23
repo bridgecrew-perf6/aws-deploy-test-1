fdf6205e1df463e22552140aea51670816ecb059
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-02-22 21:10:15 -0300

    initial commit
    Creation of the project

---
+++ src/main/java/br/com/stark/invoice/invoiceTrial/InvoiceTrialApplication.java
@@ -0,0 +1,13 @@
+package br.com.stark.invoice.invoiceTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stark/invoice/invoiceTrial/InvoiceTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stark.invoice.invoiceTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
